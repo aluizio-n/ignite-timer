028da72bec080f4801f72e0755e8d935d8507103
Aluizio N <aluizio.sln@gmail.com>
2023-01-10 16:31:08 -0400

    feat: styled component type and ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-
-
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
 
 function App() {
-  
-
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
